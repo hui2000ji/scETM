a6bbb3a7b6ef7e438ce719dd7594a9e2ea87b25f
hui2000ji <391085296@qq.com>
2021-05-13 18:59:28 +0800

    update eval mouse (plot)

--- scripts/eval_mouse.py
+++ scripts/eval_mouse.py
@@ -1,5 +1,6 @@
 import argparse
 import anndata
+from pathlib import Path
 
 from scETM import evaluate
 
@@ -8,4 +9,4 @@ parser.add_argument('--h5ad-path')
 args = parser.parse_args()
 
 adata = anndata.read_h5ad(args.h5ad_path)
-evaluate(adata, embedding_key='X', resolutions=[0.1, 0.15, 0.22, 0.34, 0.51, 0.76])
+evaluate(adata, embedding_key='X', resolutions=[0.1, 0.15, 0.22, 0.34, 0.51, 0.76], plot_dir=str(Path(args.h5ad_path).parent))
